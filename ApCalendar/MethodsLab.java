25bce2ad94d4bae4109c5f57f9257943a43cda3f
SteveChoi1219 <stevechoi268@gmail.com>
2024-11-14 17:48:02 -0500

    lab complete

--- ApCalendar/MethodsLab.java
+++ ApCalendar/MethodsLab.java
@@ -6,19 +6,37 @@ public class MethodsLab {
         System.out.println();
     }
     public static void whileLoop(int number) {
-      int counter = 0;
-      int total = 0;
-      String s = Integer.toString(number);
-      while(counter < s.length()) {
-        
-      }
+        int digitCount = 0;
+        int digitSum = 0;
+
+        while(number > 0) {
+            int digit = number % 10;
+            digitSum += digit;
+            digitCount++;
+            number /= 10;
+        }
+        System.out.println(digitCount + " digits totaling " + digitSum);
     }
+
+    public static void addHyphen(String input) {
+        System.out.println(input);
+        String upperCaseString = input.toUpperCase();
+        System.out.println(upperCaseString);
+        String hyphenated = upperCaseString.replace(" ", "-");
+        System.out.println(hyphenated);
+
+    }
+
     public static void main(String[] args) {
         forLoop(2, 90, 5);
         forLoop(3, 74, 5);
         forLoop(6, 12, 1);
 
-        
+        whileLoop(100);
+        whileLoop(3333);
+
+        addHyphen("hello world");
+        addHyphen("My name is Steve");
 
     }
 }
